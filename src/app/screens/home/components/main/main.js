678c77e5347ea66a9faede34de5e279cc7a9597a
Breyner <mon2bdr@gmail.com>
2020-07-01 14:54:44 -0500

    Add styles to header and customize navbar

--- src/app/screens/home/components/main/main.js
+++ src/app/screens/home/components/main/main.js
@@ -3,12 +3,8 @@ import './main.sass';
 
 const MainHomeComponent = () => {
     return (
-        <main className="home-main" id="who-we-are">
-            <p>SCROLL DOWN</p>
-            <p>&#8615;</p>
-            <p>&#8615;</p>
-            <p>&#8615;</p>
-            <p>&#8615;</p>
+        <main className="home-main">
+            Lorem ipsum
         </main>
     );
 }
--- src/app/screens/home/components/navbar/navbar.js
+++ src/app/screens/home/components/navbar/navbar.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './navbar.sass';
 import LanguageComponent from '../../../../components/language/language';
 import { Navbar, Nav } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
 const NavbarHomeComponent = () => {
+    const [isTop, setIsTop] = useState(1);
+
+    useEffect(() => {
+        document.addEventListener('scroll', () => {
+            const scrollCheck = window.scrollY < 100;
+            if (scrollCheck !== isTop) {
+                setIsTop(scrollCheck);
+            }
+        })
+    }, [])
+
     const [t] = useTranslation();
     return (
-        <div>
-            <Navbar expand="lg" className="home-navbar">
-                <Navbar.Brand href="#home" className="logo">Ubeslang</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href='/'>{t('navbar.menu.home')}</Nav.Link>
-                        <Nav.Link href='/#tools'>{t('navbar.menu.tools')}</Nav.Link>
-                        <Nav.Link href='/#who-we-are'>{t('navbar.menu.whoweare')}</Nav.Link>
-                        <Nav.Link href='/changelog'>{t('navbar.menu.changelog')}</Nav.Link>
-                        <Nav.Link href='/dashboard'>{t('navbar.menu.dashboard')}</Nav.Link>
-                    </Nav>
-                    <LanguageComponent />
-                </Navbar.Collapse>
-            </Navbar>
-        </div>
+        <Navbar fixed="top" expand="lg" className={"custom-home-navbar " + (isTop ? "home-navbar-top" : "home-navbar-normal")}>
+            <Navbar.Brand href="#home" className="logo">Ubeslang</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link href='/'>{t('navbar.menu.home')}</Nav.Link>
+                    <Nav.Link href='/#tools'>{t('navbar.menu.tools')}</Nav.Link>
+                    <Nav.Link href='/#who-we-are'>{t('navbar.menu.whoweare')}</Nav.Link>
+                    <Nav.Link href='/changelog'>{t('navbar.menu.changelog')}</Nav.Link>
+                    <Nav.Link href='/dashboard'>{t('navbar.menu.dashboard')}</Nav.Link>
+                </Nav>
+                <LanguageComponent />
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
 
